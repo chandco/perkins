db0bbe505146e343b652e5fdf313833fb57bf846
Nathan <cowfields@gmail.com>
2014-11-12 13:48:52 +0000

    hopefully fixed

--- js/main.js
+++ js/main.js
@@ -23,43 +23,7 @@ jQuery(function($) {
     });
 
 
-    // fix for broken iphones
-    function iOSversion() {
-      if (/iP(hone|od|ad)/.test(navigator.platform)) {
-        // supports iOS 2.0 and later: <http://bit.ly/TJjs1V>
-        var v = (navigator.appVersion).match(/OS (\d+)_(\d+)_?(\d+)?/);
-        return [parseInt(v[1], 10), parseInt(v[2], 10), parseInt(v[3] || 0, 10)];
-      } else {
-        return false; // not on IOS
-      }
-    }
-    ver = iOSversion();
-    console.log(ver);
-    if(1) { //ver[0] < 8 && ver[0]){
 
-        console.log('ios version below 8');
-
-        $("body.home #main-header").css( 'height', ( 0.88 * 800 ) );
-        $("body.home #menu-container").css('min-height',( 0.12 * 800 ));
-
-        $("body:not(.home) #main-header").css('height', '400px');
-        $("body:not(.home) #header").css('height', '200px').css("top",'60px');
-        $("body:not(.home) #menu-container").css('min-height','80px');
-
-        function iosVhHeightBug() {
-
-            $("body.home #main-header").css( 'height', ( 0.88 * 800 ) );
-            $("body.home #menu-container").css('min-height',( 0.12 * 800 ));
-
-            $("body:not(.home) #main-header").css('height', '400px');
-            $("body:not(.home) #header").css('height', '200px').css("top",'60px');
-            $("body:not(.home) #menu-container").css('min-height','80px');
-        }
-
-        iosVhHeightBug();
-        $(window).bind('resize', iosVhHeightBug);
-
-    }  
 
 
     var popup = {
