d0bd83ac4a656e426615e4bc5c02b55ff1bbbcd1
U-CHARLTONHOUSE\nathan.edwards <nathan.edwards@NATHANEDWARD-LT.charltonhouse.co.uk>
2014-11-13 16:26:50 +0000

    IE fix for content page carousel

--- header.php
+++ header.php
@@ -10,9 +10,7 @@
  * @since Twenty Eleven 1.0
  */
 
-?>
-
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+?><!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 
 <!--[if IE 6]>
 
